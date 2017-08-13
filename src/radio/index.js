9e217a7b59f44e24445b134a8ab51aaa2dae2587
Keira <k3ira.now@gmail.com>
2017-08-13 14:00:40 +0200

    Fix Renamings

--- src/radio/index.js
+++
@@ -1,4 +0,0 @@
-export {XRadioButton} from './RadioButton';
-export {XRadioGroup} from './RadioGroup';
-
-export default './RadioButton';
--- src/toaster/index.js
+++
@@ -1,4 +0,0 @@
-export {Toast} from './XToast';
-export {Toaster} from './XToaster';
-
-export default './XToast';
